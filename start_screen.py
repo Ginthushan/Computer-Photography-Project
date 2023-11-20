e6ab9e48f15f87e89213d048640ed9d281689b41
Ginthushan <90259988+Ginthushan@users.noreply.github.com>
2023-11-20 18:22:14 -0500

    Update start_screen.py

--- start_screen.py
+++ start_screen.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from main_screen import main_screen
 
 def start_screen():
     layout = [
@@ -16,7 +17,8 @@ def start_screen():
         if event == sg.WIN_CLOSED:
             break
         elif event == "-START-":
-            ...
+            window.close()
+            main_screen()
         elif event == "-ABOUT-":
             sg.popup("Photoshop Clone\nVersion 1.0\n\nDeveloped by Ginthushan Kandasamy & Dennis Peng")
 
